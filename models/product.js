3e51b911f3ad95b5dea8e3e4ce40d3318d849704
Shannon Mulloy <student@epicodus.com>
2015-12-07 16:21:42 -0800

    create mongoose models for amazon clone

---
+++ models/product.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+var Category = require('./category');
+
+var productSchema = {
+  name: { type: String, required: true },
+  pictures: [{ type: String, match: /^http:\/\//i }],
+  price: {
+    amount: { type: NUmber, required: true },
+    currency: {
+      type: String,
+      enum: ['USD', 'EUR', 'GBP'],
+      required: true
+    }
+  },
+  category: Category.categorySchema
+};
+
+module.exports = new mongoose.Schema(productSchema);
+module.exports.productSchema = productSchema;
+// ^ for putting into users' carts
